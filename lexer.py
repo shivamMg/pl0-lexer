ba16571c3999125c6b177a632deef3f8fb2b2bf2
Shivam Mamgain <shivamMg@users.noreply.github.com>
2018-08-09 10:28:18 +0530

    Add basic PL0Lexer

---
+++ lexer.py
@@ -0,0 +1,29 @@
+import re
+
+from pygments.lexer import RegexLexer, words
+from pygments.token import Text, Keyword, Punctuation, Operator, Name
+
+__all__ = ['PL0Lexer']
+
+
+class PL0Lexer(RegexLexer):
+    name = 'PL/0'
+    filenames = ['*.pl0']
+    aliases = ['pl0']
+    mimetypes = ['text/x-pl0src']
+
+    flags = re.IGNORECASE
+
+    tokens = {
+        'root': [
+            (r'\n', Text),
+            (r'\s+', Text),
+			(r'(var|procedure|const)\b', Keyword.Declaration),
+            (words(('call', 'begin', 'end', 'if', 'then', 'while', 'do',
+            'odd'), suffix=r'\b'), Keyword),
+            (r'[.,;!?()]', Punctuation),
+            (r'(:=|=|#|<|<=|>|>=|[+\-*/])', Operator),
+            # ident
+            (r'[^\W\d]\w*', Name.Other),
+        ]
+    }
